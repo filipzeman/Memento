82e4f1672660192df7fc2ef2dd6bbe496466610e
Filip Zeman <zeman.filip@gmail.com>
2021-06-18 15:45:39 +0200

    Initial Commit

---
+++ Memento/MementoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MementoApp.swift
+//  Memento
+//
+//  Created by Filip Zeman on 18.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct MementoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
